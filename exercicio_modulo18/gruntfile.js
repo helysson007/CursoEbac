ebbc0c0bd09f52aa449ed78343010c7ac4396723
Helysson Pinheiro <helyssonpinheiro@gmail.com>
2023-05-13 19:09:09 -0300

    finalizando exercicio

---
+++ exercicio_modulo18/gruntfile.js
@@ -0,0 +1,32 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        less: {
+            development: {
+                files: {
+                    'dev/styles/main.css': 'src/styles/main.less'
+                }
+            }
+        },
+        watch: {
+            less: {
+                files: ['src/styles/**/*.less'],
+                tasks: ['less:development']
+            }
+        },
+        uglify: {
+            target: {
+                files: {
+                    'dev/scripts/main.min.js': 'src/scripts/main.js'
+                }
+            }
+        }
+    })
+
+    grunt.loadNpmTasks('grunt-contrib-less')
+    grunt.loadNpmTasks('grunt-contrib-watch')
+    grunt.loadNpmTasks('grunt-contrib-uglify')
+
+    grunt.registerTask('default', ['watch'])
+    grunt.registerTask('build', ['less:development', 'uglify'])
+}
